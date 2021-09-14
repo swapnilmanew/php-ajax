8a4ea2185b9b8219a7049dc758d59eee10fb422b
Swapnil Mane <swapnilmanew@gmail.com>
2021-09-14 09:37:08 +0600

    first commit

---
+++ php/backend.php
@@ -0,0 +1,39 @@
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "student";
+    $con = mysqli_connect($host, $username,$password,$db_name);
+    extract($_POST);
+
+    // inserting data into the database
+        if(isset($_POST['studentName']) || isset($_POST['age']) || isset($_POST['city']) || isset($_FILES) )
+            {
+            $filename = $_FILES["image"]["name"];
+            $tmp_name = $_FILES["image"]["tmp_name"];
+            $path = "images/".$filename;
+            move_uploaded_file($tmp_name, $path);
+            
+            $insert_query = "INSERT INTO data VALUES(' ', '$studentName', '$age', '$city', '$filename')";
+            $fire_query = mysqli_query($con,$insert_query);
+            }
+
+        //fetching data from the database
+        if(isset($_POST['records']))
+            {
+                $display_query = "SELECT * FROM data order by id desc LIMIT 1";
+                $display_fire = mysqli_query($con,$display_query);
+                $result = mysqli_fetch_assoc($display_fire);
+                echo "
+                <div class='card p-3'>
+                <img src='./images/".$result['image']."' alt=''>
+                <div class='card-body'>
+                <h3>Name : ".$result['name']."</h3>
+                <h3>Age : ".$result['age']."</h3>
+                <h3>City : ".$result['city']."</h3>
+                </div>
+                </div>
+                    ";
+        }
+
+?>
---
+++ php/index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>Interview Question</title>
+  </head>
+  <body>
+
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-6 col-md-6 mx-auto my-4">
+          <div class="card p-3">
+            <div class="card-body">
+              <div class="card-title display-4 text-center">
+                Add Student
+              </div>
+              <hr>
+            <form id="form" action="" method="POST" enctype="multipart/form-data">
+            <input type="text" name="studentName" id="studentName" class="form-control my-3" placeholder="Student Name">
+            <input type="text" name="age" id="age" class="form-control my-3" placeholder="Age">
+            <input type="text" name="city" id="city" class="form-control my-3" placeholder="City">
+            <input type="file" name="image" id="image" class="form-control my-3">
+            <button class="btn btn-primary" type="submit" id="submitbtn">Submit</button>
+          </forom>
+          </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 mx-auto my-4">
+         <div class="showData"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Jquery CDN -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  
+  <script>
+
+ 
+    //  fetching and displaying data from the database
+      const records = "records";
+      function displayData()
+    { 
+        $.ajax({
+          url:"backend.php",
+          type:"POST",
+          data:{records:records},
+          success:function(data, status)
+          {
+            $(".showData").html(data);
+          }
+        })
+    }
+
+    //  inserting data into the database
+      $("#form").on("submit", function(event){
+        event.preventDefault();
+        $.ajax({
+          url:"backend.php",
+          type: "POST",
+          data : new FormData(this),
+          contentType:false,
+          processData:false,
+          success:function(data,status){
+            displayData();
+          }
+        })
+      })
+
+    
+  </script>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+  </body>
+</html>
